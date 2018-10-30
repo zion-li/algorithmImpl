47090e9766f12518a26e530f3ecb72073ed066c9
李佳 <zion.li@baifendian.com>
2018-10-30 13:28:56 +0800

    希尔排序实现

---
+++ src/main/java/com/myself/sort/ShellSort.java
@@ -0,0 +1,28 @@
+package com.myself.sort;
+
+import com.myself.utils.Sweep;
+
+/**
+ * 希尔排序
+ * 希尔排序是非稳定排序算法
+ * @author Created by zion
+ * @Date 2018/10/30.
+ */
+public class ShellSort {
+
+    public void shellSort(int[] array, int n) {
+        //增量
+        int incrementNum = array.length / 2;
+        while (incrementNum >= 1) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = i; j < array.length - incrementNum; j += incrementNum) {
+                    if (array[j] > array[j + incrementNum]) {
+                        Sweep.sweep(array, j, j + incrementNum);
+                    }
+                }
+            }
+            //设置新的增量
+            incrementNum = incrementNum / 2;
+        }
+    }
+}
---
+++ src/test/java/com/myself/sort/ShellSortTest.java
@@ -0,0 +1,24 @@
+package com.myself.sort;
+
+import com.myself.utils.GenerateRandom;
+import org.junit.Test;
+
+/**
+ * @author Created by zion
+ * @Date 2018/10/30.
+ */
+public class ShellSortTest {
+    @Test
+    public void shellSort() throws Exception {
+        ShellSort shellSort=new ShellSort();
+        long startTime = System.currentTimeMillis();
+        int[] input = GenerateRandom.generateRandomArray(10, 1, 100);
+        shellSort.shellSort(input, input.length);
+        long endTime = System.currentTimeMillis();
+        for (int anInput : input) {
+            System.out.println(anInput);
+        }
+        System.out.println("used time 1673 :" + (endTime - startTime));
+    }
+
+}
